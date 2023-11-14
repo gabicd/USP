a9a7e43bf659682e69940a370e28370e43daf1bb
gabicd <131476835+gabicd@users.noreply.github.com>
2023-11-14 16:17:21 -0300

    Add files via upload

---
+++ Introdução à Ciência de Computação II/atv8.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+int pesquisar(int *vetor, int pesquisa, int n);
+void ShellSort(int v[], int n);
+
+int main (){
+int n, k;
+scanf("%d", &n);
+int *vetor = (int *)malloc(n * sizeof(int));
+
+for (int i = 0; i < n; i++)
+{
+    scanf("%d", &vetor[i]);
+}
+
+scanf("%d", &k);
+int *pesquisa = (int *)malloc(k * sizeof(int));
+
+for (int i = 0; i < k; i++)
+{
+    scanf("%d", &pesquisa[i]);
+}
+
+for (int i = 0; i < k; i++)
+{
+   pesquisar(vetor, pesquisa[i], n);
+}
+
+    free(vetor);
+    free(pesquisa); 
+
+    return 0;
+
+}
+
+int pesquisar(int *vetor, int pesquisa, int n){
+    int i = 0;
+
+	while (i != n) {
+		if (vetor[i] == pesquisa){
+			return puts("1");
+            break;
+        }
+
+		i++;
+	}
+	return puts("0");
+}
