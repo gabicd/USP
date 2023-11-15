45088ff537d8f1aec137b5f6568d5ebad53b27ab
gabicd <131476835+gabicd@users.noreply.github.com>
2023-11-14 21:20:13 -0300

    versao final atv8.c

--- Introdução à Ciência de Computação II/atv8.c
+++ Introdução à Ciência de Computação II/atv8.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-int pesquisar(int *vetor, int pesquisa, int n);
+
+int pesquisar(int *vetor, int l, int r, int pesquisa);
+void ShellSort(int v[], int n);
 
 int main (){
 int n, k;
@@ -12,6 +14,8 @@ for (int i = 0; i < n; i++)
     scanf("%d", &vetor[i]);
 }
 
+ShellSort(vetor, n);
+
 scanf("%d", &k);
 int *pesquisa = (int *)malloc(k * sizeof(int));
 
@@ -20,10 +24,10 @@ for (int i = 0; i < k; i++)
     scanf("%d", &pesquisa[i]);
 }
 
-for (int i = 0; i < k; i++)
-{
-   pesquisar(vetor, pesquisa[i], n);
-}
+    for (int i = 0; i < k; i++)
+    {
+        pesquisar(vetor, 0, n-1, pesquisa[i]);
+    }
 
     free(vetor);
     free(pesquisa); 
@@ -32,16 +36,57 @@ for (int i = 0; i < k; i++)
 
 }
 
-int pesquisar(int *vetor, int pesquisa, int n){
-    int i = 0;
+int pesquisar(int *vetor, int l, int r, int pesquisa){
+    	while (l <= r) {
+		int m = l + (r - l) / 2;
 
-	while (i != n) {
-		if (vetor[i] == pesquisa){
+		if (vetor[m] == pesquisa)
 			return puts("1");
-            break;
+
+		if (vetor[m] < pesquisa)
+			l = m + 1;
+
+		else
+			r = m - 1;
+	}
+
+	return puts("0");
+}
+
+void ShellSort(int v[], int n)
+{
+
+    int gap = 1;
+
+    while (gap <= n)
+    {
+        gap *= 2;
+    }
+
+    gap = (gap / 2) - 1;
+
+    while (gap > 0)
+    {
+        for (int i = gap; i < n; i++)
+        {
+            int x = v[i];
+            int j = i - gap;
+
+            while (j >= 0)
+            {
+                if( v[j] <= x )
+                {                    
+                    break;
+                }
+
+                v[j + gap] = v[j];
+                j -= gap;
+            }
+
+            v[j + gap] = x;
+            
         }
 
-		i++;
-	}
-	return puts("0");
+        gap /= 2;
+    }
 }
