559835ba49e982a95e046a1f231756f0fef80442
gabicd <131476835+gabicd@users.noreply.github.com>
2023-11-27 20:35:21 -0300

    logica da inserção atv9.c

--- Introdução à Ciência de Computação II/atv9.c
+++ Introdução à Ciência de Computação II/atv9.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct 
-{
-    unsigned int chave;
+typedef struct {
+    int chave;
     unsigned int elemento;
-} t_elemento;
+}
+t_elemento;
 
-int hashIndex(unsigned int chave, int m);
-void hashInsert(unsigned int chave, unsigned int elemento, t_elemento *hashVetor, int m);
-
+int hashIndex(unsigned int elemento, int m);
+int hashInsert(unsigned int elemento,t_elemento hashVetor[], int m);
+int hashSearch(t_elemento hashVetor[], unsigned int elemento);
 
 int main (){
-unsigned int chave, elemento;
+unsigned int elemento;
 int m, n, d, b;
 scanf("%d", &m);
 t_elemento *hashVetor = (t_elemento *)malloc(m * sizeof(t_elemento));   //cria o vetor da hash table e aloca a memoria necessaria
+
+for (int i = 0; i < m; i++)
+{
+    hashVetor[i].chave = -1;
+    hashVetor[i].elemento = 0;
+}
+
 scanf("%d", &n);
+
 for (int i = 0; i < n; i++)
 {
-    scanf("%u", &chave);
     scanf("%u", &elemento);
-    hashInsert(chave, elemento, hashVetor, m);
+    hashInsert(elemento, hashVetor, m);
 }
 
 for (int i = 0; i < m; i++){
@@ -32,16 +39,27 @@ return 0;
 
 }
 
-void hashInsert(unsigned int chave, unsigned int elemento, t_elemento *hashVetor, int m){
+int hashIndex(unsigned int elemento, int m){
+    return (elemento % m);
+}
 
-    int index = hashIndex(chave, elemento);
+int hashInsert(unsigned int elemento,t_elemento hashVetor[], int m){
+    int index = hashIndex(elemento, m);
     
-    hashVetor[index].chave = chave;
+    for (int i = 0; i < m; i++)
+    {
+        if (hashVetor[i].chave == index && hashVetor[i].elemento != elemento)
+        {
+            index+=1;
+        }
+        
+        if (hashVetor[i].chave == index && hashVetor[i].elemento == elemento){
+            return 0;
+        }
+    }
+    
+    hashVetor[index].chave = index;
     hashVetor[index].elemento = elemento;
 
 }
 
-
-int hashIndex(unsigned int chave, int m){
-    return (chave % m);
-}
