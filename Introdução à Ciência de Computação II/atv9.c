39b7ad6bf01e9ebd83e6580b0327585f981ea617
gabicd <131476835+gabicd@users.noreply.github.com>
2023-11-28 00:12:11 -0300

    funcionando atv9.c

--- Introdução à Ciência de Computação II/atv9.c
+++ Introdução à Ciência de Computação II/atv9.c
@@ -3,16 +3,17 @@
 
 typedef struct {
     int chave;
-    unsigned int elemento;
+    int elemento;
 }
 t_elemento;
 
-int hashIndex(unsigned int elemento, int m);
-int hashInsert(unsigned int elemento,t_elemento hashVetor[], int m);
-int hashSearch(t_elemento hashVetor[], unsigned int elemento);
+int hashIndex(int elemento, int m);
+int hashInsert(int elemento,t_elemento hashVetor[], int m);
+int hashSearch(t_elemento hashVetor[], int elemento, int m);
+int hashRemove(t_elemento hashVetor[], int elemento, int m);
 
 int main (){
-unsigned int elemento;
+int elemento;
 int m, n, d, b;
 scanf("%d", &m);
 t_elemento *hashVetor = (t_elemento *)malloc(m * sizeof(t_elemento));   //cria o vetor da hash table e aloca a memoria necessaria
@@ -20,30 +21,58 @@ t_elemento *hashVetor = (t_elemento *)malloc(m * sizeof(t_elemento));   //cria o
 for (int i = 0; i < m; i++)
 {
     hashVetor[i].chave = -1;
-    hashVetor[i].elemento = 0;
+    hashVetor[i].elemento = -1;
 }
 
 scanf("%d", &n);
 
 for (int i = 0; i < n; i++)
 {
-    scanf("%u", &elemento);
+    scanf("%d", &elemento);
     hashInsert(elemento, hashVetor, m);
 }
 
-for (int i = 0; i < m; i++){
-    printf("%d %u\n", i, hashVetor[i].elemento);
+scanf("%d", &d);
+int *remover = (int *)malloc(d *sizeof(int));
+for (int i = 0; i < d; i++)
+{
+scanf("%d", &remover[i]);
 }
+
+for (int i = 0; i < d; i++)
+{
+   hashRemove(hashVetor, remover[i], m);
+   
+}
+
+scanf("%d", &b);
+int *busca = (int *)malloc(b *sizeof(int));
+for (int i = 0; i < b; i++)
+{
+scanf("%d", &busca[i]);
+}
+
+for (int i = 0; i < b; i++)
+{
+    int resultado = hashSearch(hashVetor, busca[i], m);
+    printf("%d ", resultado);
+}
+
+printf("\n"); 
+
+free(hashVetor);
+free(busca);
+free(remover);
 
 return 0;
 
 }
 
-int hashIndex(unsigned int elemento, int m){
+int hashIndex(int elemento, int m){
     return (elemento % m);
 }
 
-int hashInsert(unsigned int elemento,t_elemento hashVetor[], int m){
+int hashInsert(int elemento,t_elemento hashVetor[], int m){
     int index = hashIndex(elemento, m);
     
     for (int i = 0; i < m; i++)
@@ -63,3 +92,25 @@ int hashInsert(unsigned int elemento,t_elemento hashVetor[], int m){
 
 }
 
+int hashSearch(t_elemento hashVetor[], int elemento, int m){
+    int index = hashIndex(elemento, m);
+
+    if (elemento == hashVetor[index].elemento)
+    {
+        return index;
+    }
+    
+    else{
+        return -1;
+    }
+
+}
+
+int hashRemove(t_elemento hashVetor[], int elemento, int m){
+    int pesquisa = hashSearch(hashVetor, elemento, m);
+    if (pesquisa != -1)
+    {
+        hashVetor[pesquisa].elemento = -1;
+    }
+    return 0;
+}
