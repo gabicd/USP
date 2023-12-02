c1f478c897690e967e7f8d87254b839f32f15404
gabicd <131476835+gabicd@users.noreply.github.com>
2023-12-01 21:35:46 -0300

    Update atv10.c
    falta recursão

--- Introdução à Ciência de Computação II/atv10.c
+++ Introdução à Ciência de Computação II/atv10.c
@@ -4,17 +4,20 @@
 
 #define MAX 128
 
+char* concatstr(char* read1, char* read2);
+int compstring(char* read1, char* read2, int size);
+char* melhoroverlap(char** conjuntoreads, int n);
 int permutacao(int n);
 int overlapsize(char* read1, char* read2);
 char* overlapstring(char* read1, char* read2, int size);
 char* maiorstr (char* read1, char* read2);
 
+int r1, r2;
+
 int main (){
 int n;
 scanf("%d", &n);
 char **conjuntoreads = (char **)malloc(n * sizeof(char*));
-int np = permutacao(n);
-int *qovps = (int *)malloc(np * sizeof(int));
 
 int j = 0;
 for (int i = 0; i < n; i++)
@@ -26,23 +29,7 @@ for (int i = 0; i < n; i++)
     strcpy(conjuntoreads[i], reads);
 }
 
-int moverlap = 0;
-int r1, r2;
-for (int i = 0; i < n; i++)
-{
-    for (int j = i+1; j < n; j++)
-    {
-        if(overlapsize(conjuntoreads[i], conjuntoreads[j])> moverlap){
-            moverlap = overlapsize(conjuntoreads[i], conjuntoreads[j]);
-            r1=i;
-            r2 =j;
-        }
-        
-    }
-}
-
-printf("%s\n", overlapstring(conjuntoreads[r1], conjuntoreads[r2],moverlap));
-
+    printf("%s\n", melhoroverlap(conjuntoreads, n));
 
 for (int i = 0; i < n; i++)
 {
@@ -53,23 +40,27 @@ return 0;
 
 }
 
+int compstring(char* read1, char* read2, int size){
+char* sufixo = (char *)malloc((size+1) * sizeof(char));
+for (int i = 0; i < size; i++)
+{
+    sufixo[i] = read1[i+(strlen(read1)-size)];
+}
+
+if (strncmp(sufixo, read2, size) == 0)
+{
+    return 1;
+}
+
+return -1;
+}
+
 char* maiorstr (char* read1, char* read2){
     if (strlen(read1)<strlen(read2))
     {
        return read1;   }
     
 return read2;
-}
-
-int permutacao(int n){
-int np = n;
-
-for (int i = n-1; i > 0; i--)
-{
-    np = np *i;
-}
-
-return np;
 }
 
 int overlapsize(char* read1, char* read2){
@@ -88,13 +79,28 @@ for (int i = 0; i <= strlen(read1); i++)
                 maxover = (chartcomp[i][j] > maxover) ? chartcomp[i][j] : maxover;
             } else {
                 chartcomp[i][j] = 0;
-    }
-    
-}
-
+        }
+   }
 }
 
 return maxover;
+}
+
+char* concatstr(char* read1, char* read2){
+int tamnovo = strlen(read1) + strlen(read2) + 1;
+char* novastr = (char*)malloc(tamnovo * sizeof(char));
+
+for (int i = 0; i < strlen(read1); i++)
+{
+    novastr[i] = read1[i];
+}
+
+for (int i = 0; i < strlen(read2); i++)
+{
+    novastr[i+strlen(read1)] = read2[i];
+}
+
+return novastr;
 }
 
 char* overlapstring(char* read1, char* read2, int size){
@@ -102,6 +108,7 @@ int tamfinal = strlen(read1) + strlen(read2) + 1;
 char* combinacao = (char *)malloc(tamfinal * sizeof(char));
 
 int fixo = strncmp(read1, read2, size);
+int suf = compstring(read1, read2, size);
     if (fixo == 0)
     {
         for (int i = 0; i < strlen(read2); i++)
@@ -113,9 +120,11 @@ int fixo = strncmp(read1, read2, size);
         {
             combinacao[i+(strlen(read2))] = read1[i+size];
         }
-    }
-    
-    else{
+    } 
+
+    else if (suf == 1)
+     {
+        //printf("%d\n", fixo);
         for (int i = 0; i < strlen(read1); i++)
         {
             combinacao[i] = read1[i];
@@ -128,5 +137,31 @@ int fixo = strncmp(read1, read2, size);
 
     }
 
+    else if (size == 0)
+    {
+        combinacao = maiorstr(read1, read2);
+    }
+
+    else
+    combinacao = concatstr(read1,read2);
+    
     return combinacao;
+}
+
+char* melhoroverlap(char** conjuntoreads, int n){
+    int moverlap = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i+1; j < n; j++)
+            {
+                if(overlapsize(conjuntoreads[i], conjuntoreads[j])> moverlap){
+                    moverlap = overlapsize(conjuntoreads[i], conjuntoreads[j]);
+                    r1=i;
+                    r2 =j;
+                }
+                
+            }
+        }
+
+    return overlapstring(conjuntoreads[r1], conjuntoreads[r2], moverlap);
 }
