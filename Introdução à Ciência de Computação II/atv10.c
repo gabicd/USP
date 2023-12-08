837d2f1ea35c7f4a8e5d4c6cd01cfc7d3515cb9f
gabicd <131476835+gabicd@users.noreply.github.com>
2023-12-07 23:46:53 -0300

    Update atv10.c
    Sem recursão aplicada, passa pelo valgrind sem erros

--- Introdução à Ciência de Computação II/atv10.c
+++ Introdução à Ciência de Computação II/atv10.c
@@ -2,54 +2,52 @@
 #include<stdlib.h>
 #include<string.h>
 
-#define MAX 128
+#define MAX 16
 
-char* acharoverlap(char** conjuntoreads, int n);
+char* acharoverlap(char** conjuntoreads, int n, char* overlap);
 int overlapsize(char* read1, char* read2);
-char* overlapstring(char* read1, char* read2, int size);
+char* overlapstring(char* read1, char* read2, int size, char* overlap);
 char* superstring(char* read1, char* read2, char* overlap, int size);
-char* concatstr(char* read1, char* read2);
 char* maiorstr (char* read1, char* read2);
 
 int r1, r2;
 
 int main (){
 int n;
-int t_aux;
 int t_ov;
 scanf("%d", &n);
 char **conjuntoreads = (char **)calloc(n, sizeof(char*));
-int* tamreads = (int *)calloc(n, sizeof(int));
-char *reads = (char *)calloc(MAX, sizeof(char));
-
 
 for (int i = 0; i < n; i++)
 {    
-    scanf("%s", reads);
-    tamreads[i] = strlen(reads);
-    conjuntoreads[i] = (char *)calloc((tamreads[i]+1), sizeof(char));
-    strncpy(conjuntoreads[i], reads, tamreads[i]);
+    conjuntoreads[i] = (char *)calloc(MAX, sizeof(char));
 }
 
-    free(reads);
-    free(tamreads); 
+for (int i = 0; i < n; i++)
+{    
+    scanf("%s", conjuntoreads[i]);  
+}
     
-    char *auxstr;
-    char* strfinal;
+    char *auxstr = (char*)calloc(MAX, sizeof(char));
+    char *strfinal;
 
-        auxstr = acharoverlap(conjuntoreads, n);
+        auxstr = acharoverlap(conjuntoreads, n, auxstr);
         t_ov = overlapsize(conjuntoreads[r1], conjuntoreads[r2]);
         strfinal = superstring(conjuntoreads[r1], conjuntoreads[r2], auxstr, t_ov);
-        
+
     printf("%s\n", strfinal);
     
     for (int i = 0; i < n; i++)
     {
-        conjuntoreads[i] = NULL;
-        free(conjuntoreads[i]);
+       if (conjuntoreads[i] != NULL)
+       {
+            free(conjuntoreads[i]);        
+       }
+        
     }
     
     free(strfinal);
+    free(auxstr);
     free(conjuntoreads);
 
 return 0;
@@ -81,12 +79,12 @@ for (int i = 0; i < t1; i++)
 return maxover;
 }
 
-char* overlapstring(char* read1, char* read2,int size){
+char* overlapstring(char* read1, char* read2,int size, char* overlap){
 int t1 = strlen(read1)+1;
 int t2 = strlen(read2)+1;
 int k = 0;
 int chartcomp[t1][t2];
-char* overlap = (char*)malloc((size+1)*sizeof(char));
+
 
 for (int i = 0; i < t1; i++)
 {
@@ -114,7 +112,7 @@ for (int i = 0; i < t1; i++)
 return overlap;
 }
 
-char* acharoverlap(char** conjuntoreads, int n){
+char* acharoverlap(char** conjuntoreads, int n, char* overlap){
     int moverlap = 0;    
         for (int i = 0; i < n; i++)
         {
@@ -128,27 +126,8 @@ char* acharoverlap(char** conjuntoreads, int n){
                 
             }
         }
-  
-    return overlapstring(conjuntoreads[r1], conjuntoreads[r2], moverlap);
-}
-
-char* concatstr(char* read1, char* read2){
-int t1 = strlen(read1);
-int t2 = strlen(read2);
-int tamnovo = t1 + t2 + 2;
-char* novastr = (char*)malloc(tamnovo * sizeof(char));
-
-for (int i = 0; i < t1; i++)
-{
-    novastr[i] = read1[i];
-}
-
-for (int i = 0; i < t2; i++)
-{
-    novastr[i+t1] = read2[i];
-}
-
-return novastr;
+    overlap = overlapstring(conjuntoreads[r1], conjuntoreads[r2], moverlap, overlap);
+    return overlap;
 }
 
 char* maiorstr (char* read1, char* read2){
@@ -163,8 +142,10 @@ return read2;
 }
 
 char* superstring(char* read1, char* read2, char* overlap, int size){
+int t1 = strlen(read1);
+int t2 = strlen(read2);
 int tamnovo = strlen(read1) + strlen(read2) + 2;
-char* sstr = (char*)malloc(tamnovo * sizeof(char));
+char* sstr = (char*)calloc(tamnovo, sizeof(char));
 int p1 = strncmp(read1, overlap, size); //read1 prefixo e read2 sufixo
 int p2 = strncmp(read2, overlap, size); //read2 prefixo e read1 sufixo
 
@@ -198,7 +179,15 @@ int p2 = strncmp(read2, overlap, size); //read2 prefixo e read1 sufixo
 
     else if (size == 0)
     {
-        sstr = concatstr(read1, read2);
+        for (int i = 0; i < t1; i++)
+        {
+            sstr[i] = read1[i];
+        }
+
+        for (int i = 0; i < t2; i++)
+        {
+            sstr[i+t1] = read2[i];
+        }
     }
     
     else
